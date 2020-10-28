0e2dab66e4763006904e2a82434e38f184373a2d
Shengyou Fan <shengyoufan@gmail.com>
2020-10-28 13:01:58 +0800

    section 1: add seeder

---
+++ database/seeders/FeedbackSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Feedback;
+use Illuminate\Database\Seeder;
+
+class FeedbackSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Feedback::truncate();
+
+        Feedback::factory(10)->create();
+    }
+}
